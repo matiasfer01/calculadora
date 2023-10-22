41fa1ee0b3df9810470c114b2101e6d65a0220ef
Luis Fernando <fer.matias.luis@gmail.com>
2023-10-21 18:45:53 -0600

    borre archivos y puse nuevos archivos

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-function appendToResult(value) {
-    document.getElementById("result").value += value;
-  }
-  
-  function clearResult() {
-    document.getElementById("result").value = "";
-  }
-  
-  function calculateResult() {
-    const result = eval(document.getElementById("result").value);
-    document.getElementById("result").value = result;
-  }
-  
+
+const numeros = document.getElementById('resultado')
+
+function pon(digito) {
+    numeros.value += digito;
+}
+
+function borrarTodo() {
+    numeros.value = '';
+}
+
+function quitar() {
+    numeros.value = numeros.value.substr(0, numeros.value.length - 1)
+}
+
+function resultado() {
+    const numeros = eval(document.getElementById("resultado").value);
+    document.getElementById("resultado").value = numeros;
+
+}
+
